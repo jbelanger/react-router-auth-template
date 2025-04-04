a53b86418b6e726533bbfe73bff2d981a5f1214f
Joel Belanger <joel.belanger@hrsdc-rhdcc.gc.ca>
2025-04-04 16:19:32 -0400

    feat(i18n): add tsconfig dependency and update routing exports

--- frontend/modules/i18n/src/routing/index.ts
+++ frontend/modules/i18n/src/routing/index.ts
@@ -2,4 +2,4 @@
 export { I18nLink } from './i18n-link.tsx';
 
 // Utilities
-export { i18nRoute, I18nRoutesProvider, useCurrentLanguage, getRouteLanguage } from './routes-utils.tsx';
+export { i18nRoute, I18nRoutesProvider, useCurrentRoute, useCurrentLanguage, getRouteLanguage } from './routes-utils.tsx';
